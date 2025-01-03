48b87a72c031d9215dc42be6d42b4d3b653a4ff8
Ronny Jahn <r.jahn@agentur-lorch.de>
2025-01-03 21:52:13 +0100

    Updated pickup address, added location

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
         <h2 class="text-2xl font-semibold mb-4">Registriere deine Kleiderspende</h2>
         <form @submit.prevent="submitDonation" class="block p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
             <div class="mb-4">
-                <label>Wie möchtest Du die Spende übergeben?</label>
+                <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Wie möchtest Du deine Kleiderspende übergeben?</label>
                 <div>
                     <label class="inline-flex items-center">
                         <input
@@ -11,9 +11,8 @@
                             class="mr-1.5 w-4 h-4 border-gray-300 focus:ring-2 focus:ring-indigo-300 dark:focus:ring-indigo-600 dark:focus:bg-indigo-600 dark:bg-gray-700 dark:border-gray-600"
                             v-model="form.deliveryOption"
                             value="office"
-                            checked
                         />
-                        Geschäftsstelle
+                        Übergabe an der Geschäftsstelle
                     </label>
                     <label class="inline-flex items-center ml-4">
                         <input
@@ -28,20 +27,30 @@
             </div>
             <div v-if="form.deliveryOption === 'pickup'" class="mb-4">
                 <div class="mb-4">
-                    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Abholadresse:<span class="text-red-600 text-sm">*</span></label>
+                    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Straße / Nr.:<span class="text-red-600 text-sm">*</span></label>
                     <input
                         type="text"
                         v-model="form.pickupAddress"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-indigo-500 dark:focus:border-indigo-500"
                     />
                 </div>
-                <div>
-                    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Postleitzahl:<span class="text-red-600 text-sm">*</span></label>
-                    <input
-                        type="text"
-                        v-model="form.pickupZip"
-                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-indigo-500 dark:focus:border-indigo-500"
-                    />
+                <div class="flex gap-4">
+                    <div class="lg:w-1/3">
+                        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Postleitzahl:<span class="text-red-600 text-sm">*</span></label>
+                        <input
+                            type="text"
+                            v-model="form.pickupZip"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-indigo-500 dark:focus:border-indigo-500"
+                        />
+                    </div>
+                    <div class="lg:w-2/3">
+                        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Ort:<span class="text-red-600 text-sm">*</span></label>
+                        <input
+                            type="text"
+                            v-model="form.pickupLocation"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-indigo-500 dark:focus:border-indigo-500"
+                        />
+                    </div>
                 </div>
             </div>
             <div class="mb-4">
@@ -78,18 +87,21 @@ const form = ref({
     deliveryOption: "office",
     pickupAddress: "",
     pickupZip: "",
+    pickupLocation: "",
     clothingType: "",
     crisisRegion: "",
 });
 const submitDonation = () => {
-    if (form.value.clothingType == "") {
+    if (form.value.deliveryOption == "pickup" && form.value.pickupAddress == "") {
+        alert("Bitte gib eine Adresse an!");
+    } else if (form.value.deliveryOption == "pickup" && form.value.pickupZip == "") {
+        alert("Bitte gib eine Postleitzahl an!");
+    } else if (form.value.deliveryOption == "pickup" && form.value.pickupLocation == "") {
+        alert("Bitte gib einen Ort an!");
+    } else if (form.value.clothingType == "") {
         alert("Bitte gib eine Art der Kleidung an!");
     } else if (form.value.crisisRegion == "") {
         alert("Bitte gib ein Krisengebiet an!");
-    } else if (form.value.deliveryOption == "pickup" && form.value.pickupAddress == "") {
-        alert("Bitte gib eine Abholadresse an!");
-    } else if (form.value.deliveryOption == "pickup" && form.value.pickupZip == "") {
-        alert("Bitte gib eine Postleitzahl an!");
     } else {
         alert(JSON.stringify(form.value, null, 2));
     }
