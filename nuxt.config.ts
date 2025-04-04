e84e1a048c8ac279f72c3348119692d22bcfb312
Ronny Jahn <r.jahn@agentur-lorch.de>
2025-04-04 10:19:54 +0200

    Updated tailwindcss to v4.1, added custom colors

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
     compatibilityDate: "2024-11-01",
     devtools: { enabled: true },
     css: ["@/assets/css/style.css"],
-    postcss: {
-        plugins: {
-            tailwindcss: {},
-            autoprefixer: {},
-        },
+    vite: {
+        plugins: [tailwindcss()],
     },
 });
